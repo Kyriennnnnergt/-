683b57381a49665cd1dc3776dbd1fd5bec723180
Kyriennnnnergt <161467782+Kyriennnnnergt@users.noreply.github.com>
2024-03-12 21:26:37 +0800

    exercise5

---
+++ script.js
@@ -0,0 +1,12 @@
+
+document.write("Hello world");
+function sayHello(){
+  var response=prompt("Who is your favorite actress?");
+  alert("fine"+response+",enjoy your own time");
+};
+//sayHello()
+function showTopic(){
+  var x = document.getElementById('demo')
+  x.style.fontSize='25px';
+  x.style.color = "red";
+};
